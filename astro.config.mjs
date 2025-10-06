8d20ec8a1154c6fd1055c561bd09ea2332f42329
Gusde Widnyana <widnyana.bagus@gmail.com>
2025-10-06 15:26:58 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://gusdeyw.github.io',
-  base: '/',
+  // No base needed for <username>.github.io repositories
 });
